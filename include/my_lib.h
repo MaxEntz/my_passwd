a4b0eb4d78d6dd6eb91d25f2997d65f150fc194f
MaxEntz <maxime.entz@epitech.eu>
2025-02-05 01:10:07 +0100

    add: the include folder, and he include some .h file, like for my lib C or for the project I modified the MakeFile because he have a coding style error xD

---
+++ include/my_lib.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** my_passwd.h
+** File description:
+** Its the header file of my_passwd project
+*/
+
+#ifndef MY_LIB_H_
+    #define MY_LIB_H_
+    #include <unistd.h>
+
+int my_strlen(char const *str);
+int my_strcmp(char const *s1, char const *s2);
+int my_putstr(char const *str);
+int my_putchar(char c);
+
+#endif /* MY_LIB_H_ */
---
+++ include/my_passwd.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** my_passwd.h
+** File description:
+** Its the header file of my_passwd project
+*/
+
+#ifndef MY_PASSWD_H_
+    #define MY_PASSWD_H_
+    #include "my_lib.h"
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <string.h>
+
+int my_passwd(int ac, char **av);
+int main_passwd(void);
+int print_help(void);
+int exit_choice(void);
+int log_in(void);
+int create_account(void);
+
+#endif /* MY_PASSWD_H_ */
